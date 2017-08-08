f36ab4537d1dde489f5829c8d63983bb2baa6324
luke <luke@pillowhomes.com>
2017-08-07 17:58:28 -0700

    Update geo locate.py

--- geolocate.py
+++ geolocate.py
@@ -1,5 +1,8 @@
 import json
 import requests
+import sys
+import pandas as pd
+import time
 
 def getRequest(address):
 	'''Returns the google api request for an address string.'''
@@ -33,7 +36,7 @@ def getPossibleLocations(address):
 	
 	for location in api_response['results']:
 		if not 'street_address' in getTypeFromJSON(location) and not 'premise' in getTypeFromJSON(location) and not 'establishment' in getTypeFromJSON(location):
-			print('Possible location is NOT a street address:')
+			print('Possible location is NOT a street address, premise, or establishment:')
 			print(getAddressFromJSON(location))
 			print('It has the following types:')
 			print(str(getTypeFromJSON(location)))
@@ -57,18 +60,66 @@ def getTypeFromJSON(json):
 def getLocation(address):
 	'''Takes an address string and returns the lat and lng in a tuple.
 	Prints irregularities found.'''
+	
+	error = None
+	result = (None, None)
+	
 	google_response = getPossibleLocations(address)
+	
 	if len(google_response) == 0:
 		print('No locations found for that address: ', address)
+		error = 'No locations found.'
+	
+	
 	elif len(google_response) > 1:
-		print('More than one possible location found for that address')
+		print('More than one possible location found for that address:')
 		for possibleLocation in google_response:
 			print(getAddressFromJSON(possibleLocation))
-		result = getGeocodeFromJSON(google_response[0])
-		return (result['lat'], result['lng'])
+		error = 'Multiple possible locations.'
+		
+		rawResult = getGeocodeFromJSON(google_response[0])
+		result = (rawResult['lat'], rawResult['lng'])
+
 	else: #Only one result found
-		result = getGeocodeFromJSON(google_response[0])
-		return (result['lat'], result['lng'])
+		rawResult = getGeocodeFromJSON(google_response[0])
+		result = (rawResult['lat'], rawResult['lng'])
+	
+	return (address, result[0], result[1], error)
+
+		
+if __name__ == '__main__':
+	# Get the file to open from the arguments
+	input_file = sys.argv[1]
+	if '.csv' not in input_file:
+		print('Please use a csv file.')
+	try:
+		input_df = pd.read_csv(input_file)
+	except:
+		print('Error in opening file.')
+	
+	
+	# Open the file and locate the address column
+	address = None
+	if 'address' not in list(input_df) and 'Address' not in list(input_df) and 'adr' not in list(input_df):
+		print('Please have at least one column labeled address')
+	
+	elif 'address' in list(input_df):
+		address = 'address'
+	elif 'Address' in list(input_df):
+		address = 'Address'
+	else:
+		address = 'adr'
+		
+		
+	# Output results
+	results = []
+	for address in input_df[address]:
+		time.sleep(.5)
+		results.append(getLocation(address))
+	
+	labels = ['Address', 'Latitude', 'Longitude', 'Error?']
+	results = pd.DataFrame.from_records(results, columns=labels)
+	results.to_csv(input_file[:-4] + '_OUTPUT.csv')
 
 # Due to biasing, '580 Market Street' and '580 Market St' return different results
 # This can be rectified by biassing towards a boundary or a region
